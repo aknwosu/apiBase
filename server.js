8dbb37e8e754e2aec5785828bc9a432d5013fa72
AK Nwosu <akunnarebecca@gmail.com>
2018-08-06 12:45:07 +0100

    set up postgres,  environments

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
-const express = require('express');
+import express from 'express';
+import logger from 'morgan';
+
+require('dotenv').config();
+
 
 const app = express();
+const { PORT } = process.env;
+
+// Log requests to the console.
+app.use(logger('dev'));
+
+// Parse incoming requests data
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello server');
 });
 
-app.listen(4000);
+app.listen(PORT);
