f0ee1c2468086f02dcd6a2afccc66d66b3bf3056
AK Nwosu <akunnarebecca@gmail.com>
2018-08-08 09:07:05 +0100

    controllers: get quotes by id, year, author

--- controllers/quotes.js
+++ controllers/quotes.js
@@ -16,3 +16,36 @@ class QuotesController {
 			return res.status(200).send({success: true, data: quotes});
 		}).catch(err => res.status(400).send({ success: false, message: err.message }));
 	}
+
+	static getQuotesById(req, res) {
+		Quote.findOne({where: {
+			id: req.params.quoteId
+		}}).then((quote) => {
+			if (!quote) {
+				return res.status(404).send({success: false, message: 'not found'});
+			}
+			return res.status(200).send({success: true, data: quote});
+		}).catch(err => res.status(400).send({ success: false, message: err.message }));
+	}
+
+	static getQuoteByAuthor(req, res) {
+		Quote.findAll({where: {
+			author: req.params.author
+		}}).then((quotes) => {
+			if (!quotes.length) {
+				return res.status(404).send({success: false, message: 'No quotes by that author found'});
+			}
+			return res.status(200).send({success: true, data: quotes});
+		}).catch(err => res.status(400).send({ success: false, message: err.message }));
+	}
+
+	static getQuoteByYear(req, res) {
+		Quote.findAll({ where: {
+			year: req.params.year
+		}}).then((quotes) => {
+			if (!quotes.length) {
+				return res.status(404).send({message: 'not found'});
+			}
+			return res.status(200).send({success: true, data: quotes});
+		}).catch(err => res.status(400).send({ success: false, message: err.message }));
+	}
