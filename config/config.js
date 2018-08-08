4fb278a07cd14d5c87a1947a01f1997c249fb366
AK Nwosu <akunnarebecca@gmail.com>
2018-08-08 09:54:06 +0100

    prod vars

--- config/config.js
+++ config/config.js
@@ -16,10 +16,10 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
+    username: process.env.PROD_USER,
+    password: process.env.PROD_PASSWORD,
+    database: process.env.PROD_NAME,
+    host: process.env.DB_HOST,
     dialect: 'postgres',
   },
 };
