224a66e5a41e11eaa6f30d02d49654f349c0efe5
AK Nwosu <akunnarebecca@gmail.com>
2018-08-08 09:15:03 +0100

    routes: get longest route, search quotes

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,17 @@ module.exports = (app) => {
 		.post(QuotesController.createQuote)
 		.get(QuotesController.getAllQuotes);
 
+	app.route('/api/v1/quotes/search')
+		.get(QuotesController.searchQuotes);
+    
 	app.route('/api/v1/quotes/:quoteId')
 		.get(QuotesController.getQuotesById)
 		.put(QuotesController.editQuote)
 		.delete(QuotesController.deleteById);
+
+	app.route('/api/v1/quotes/:quoteId/longest')
+		.get(QuotesController.getLongestWordInQuote);
+  
 	app.route('/api/v1/quotes/authors/:author')
 		.get(QuotesController.getQuoteByAuthor)
 		.delete(QuotesController.deleteByAuthor);
